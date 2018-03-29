4f4fd99320b1cf6a8aa39da6086d9c0f38361aeb
dflynn-volpe <daniel.flynn.ctr@dot.gov>
2018-03-28 22:29:29 -0400

    KDE and plotting on test cases

--- Don_3_Clusters.R
+++ Don_3_Clusters.R
@@ -5,7 +5,7 @@
 library(maps)
 library(sp)
 library(car) # for ellipse
-library(RgoogleMaps) # for plotmap and 
+library(RgoogleMaps) # for plotmap 
 library(ks)
 library(tidyverse)
 
@@ -106,3 +106,116 @@ dev.print(device = png,
 # lim <- par()
 # rasterImage(ima, lim$usr[1], lim$usr[3], lim$usr[2], lim$usr[4])
 # # grid()
+
+# KDE ----
+library(spatstat)
+# Need to define the observation window "owin" and make a point pattern process "ppp" from the lat-long points
+class(d)
+
+
+# Convert from lat long to plot-able XY for mapping
+
+ll <- LatLon2XY.centered(mm, lat = coordinates(d)[,2], lon = coordinates(d)[,1])
+
+# change f to higher value to extend the range of the observation window
+pwin = owin(xrange = extendrange(range(ll$newX), f = 0.3), 
+            yrange = extendrange(range(ll$newY), f = 0.3))
+
+
+dp <- ppp(ll$newX, ll$newY, window = pwin)
+# class(dp)
+
+dd <- density(dp,
+              kernel = "gaussian",
+              adjust = 0.8, # use this to change the bandwidth
+              edge = T, # adjustment for edge of observation window, recommend T
+              at = "pixels") # change to "points" to get the density exactly for each point
+
+mm <- plotmap(lat = dc$lat, lon = dc$lon,
+              pch = "+",
+              lwd = 0,
+              zoom = zoom,
+              maptype = "roadmap")
+
+# Define the color map:
+# library(scales); alpha(c("white", "blue", "red"), 0.5)
+
+# rev(heat.colors(15, alpha = 0.5))
+# make color map with increasing transparency at lower range
+coln = 3*25 # make it divisible by 3 for following steps
+col1 = rev(heat.colors(coln, alpha = 0.2))
+col2 = rev(heat.colors(coln, alpha = 0.5))
+col3 = rev(heat.colors(coln, alpha = 0.6))
+
+col4 = c(col1[1:coln/3], col2[(coln/3+1):(2*coln/3)], col3[(1+2*coln/3):coln])
+
+pc <- colourmap(col = col4, 
+                       range = range(dd))
+
+plot(dd, add = T, col = pc)
+
+# Get the contour levels
+levs <- quantile(dd, c(0.75, 0.9, 0.95, 1))
+# select the color for of each contour level
+legcol <- pc(levs)
+
+
+legend("topleft",
+       title = "Density",
+       legend = round(levs, 4),
+       fill = legcol)
+
+dev.print(device = png, 
+          width = 600,
+          height = 600,
+          file = paste0("Mapping_Don3_Clusters_Density.png"))
+
+
+# Re-do with kde function from ks package
+
+library(ks)
+
+fhat <- kde(x = data.frame(ll$newX, ll$newY))
+#fhat <- kde(x = data.frame(dc$lat, dc$lon))
+
+plotmap(lat = dc$lat, lon = dc$lon,
+              pch = "+",
+              lwd = 0,
+              zoom = zoom,
+              maptype = "roadmap")
+
+plot(fhat, add = T,
+     drawpoints = F,
+     display = "filled.contour"
+     )
+
+# Get the contour levels
+levs <- contourLevels(fhat)
+contcut <- cut(fhat$cont, breaks = levs)
+# select the first instance of each contour level
+colordf <- data.frame(contcut, heat.colors(99))
+legcol <- colordf[!duplicated(colordf[,1]),]
+
+legend("topleft",
+       title = "Density",
+       legend = legcol$contcut,
+       fill = as.character(legcol$heat.colors.99)
+       )
+
+
+dev.print(device = png, 
+          width = 600,
+          height = 600,
+          file = paste0("Mapping_Don3_Clusters_KDE.png"))
+
+plot(fhat, add = F,
+      display = "persp",
+     xlab = "Longitude",
+     ylab = "Latitude",
+     zlab = "Density"
+)
+
+dev.print(device = png, 
+          width = 600,
+          height = 600,
+          file = paste0("Mapping_Don3_Clusters_KDE_3d.png"))
---
+++ Route_12_kde.R
@@ -0,0 +1,140 @@
+# Pedestrian conflict hot spot analysis
+# Based on Mobileye data from vehicle in Seattle in April-June 2016
+
+# 1. Cluster using Euclidean distance + Nearest-neighbor linkage (dist + hclust)
+# 2. Kernal density using ks package or other
+
+# For each approach, make maps with satellite image overlays. Highlight bus stops, and look at associate between hot spots and bus stops.
+
+# Figures to make:
+# Weekend, weekday
+# Daylight, non-daylight (should not be many)
+# Overall
+# 
+# Make difference plots 
+# 
+# Bandwidth: within 100'? 4 lane intersection is ~ 50' wide
+# Use all warnings for now
+
+# Setup ----
+library(maps)
+library(sp)
+library(RgoogleMaps)
+library(ks)
+library(tidyverse)
+
+setwd("M:/Helping/Fisher/Crimestat") # update as appropriate 
+
+d <- read.csv("Route 12 data.csv", stringsAsFactors = F)
+
+PLOTMAPS = F # Set to T to make individual maps by StatusName
+
+# Look at the data and reformat ----
+
+summary(d)
+
+# get date and time in the right format
+d$time <- strptime(d$LocationTime, "%m/%d/%Y %H:%M:%S")
+summary(d$time)
+
+# trim trailing white space from all text fields
+textfields <- c("VehicleName", "UnitID", "Address", "StatusName")
+d[textfields] <- apply(d[textfields], 2, FUN = function(x) sub(" +$", "", x))
+
+# Make it a spatial data frame, only picking out relevant columns
+d <- SpatialPointsDataFrame(coords = d[c("Longitude","Latitude")], data = d[c("Address", "StatusName", "time")])
+
+# 1. Clustering ----
+
+# Plot all, by StatusName 
+
+if(PLOTMAPS) {
+pointcols <- rainbow(n = length(unique(d$StatusName)), v = 0.9, start = 0.7, end = .1, alpha = 0.25)
+
+for(i in 1:length(unique(d$StatusName))){
+  dx = d[d$StatusName == unique(d$StatusName)[i],]
+  
+
+  plotmap(lat = coordinates(dx)[,2], lon = coordinates(dx)[,1],
+          #zoom = 13,
+          pch = "+",
+          cex = 2,
+          col = pointcols[i],
+          maptype = "roadmap"
+          )
+  
+   title(main = paste("\n", unique(d$StatusName)[i]))
+  
+  dev.print(device = png, 
+            width = 600,
+            height = 600,
+            file = paste0("Mapping_Route12_", unique(d$StatusName)[i], ".png"))
+  
+  }
+}
+# Make clusters by spatial and temporal proximity
+
+d.dist <- spDists(d, longlat = T)*1000 # spDists gives distance in km, increasing here to m 
+# minute of day
+m.of.d <- rowSums(data.frame(
+                  as.numeric(format(d$time, "%H")),
+                  as.numeric(format(d$time, "%M"))/60,
+                  as.numeric(format(d$time, "%S"))/3600))
+
+d.temp <- as.matrix(dist(m.of.d)) # distance matrix by time. This is difference in minutes by time of day (i.e., ignoring date, just looking within each day). Need to correctly account for 24h time; currently 00:00 is max different from 23:59
+
+d.dt <- d.dist * d.temp # dim(d.dt) # consider scaling to give equal weight to location and time 
+
+d.clust <- hclust(as.dist(d.dist), method = "average") # linkage by unweighted pairwise group method with arithmatic mean, UPGMA. Change method to "single" for single linkage, see ?hclust for more options. 
+
+# Alternative: k-means clustering, using kmeans()
+
+# cut the tree to make groupings
+
+d.cut <- cutree(d.clust, k = 10) # return the 10 top level groups.
+
+dc <- data.frame(d@data, d.cut, lat = coordinates(d)[,2], lon = coordinates(d)[,1], m.of.d)
+
+dc$time <- as.character(dc$time) # dplyr doesn't work well with date-time formats
+
+# plot(as.dendrogram(d.clust))
+# rect.hclust(d.clust, k = 10)
+
+# aggregated by group and summarize. can add StatusName to group_by
+dc2 <- dc %>%
+  group_by(d.cut) %>%
+  summarize(lat.m = mean(lat),
+            lon.m = mean(lon),
+            lat.sd = sd(lat),
+            lon.sd = sd(lon),
+            n = n(),
+            time = mean(m.of.d),
+            time.sd = sd(m.of.d)
+            )
+
+mm <- plotmap(lat = dc2$lat.m, lon = dc2$lon.m,
+        pch = 21,
+        bg = "grey80",
+        lwd = 2,
+        cex = dc2$n/50,
+        col = "red",
+        maptype = "roadmap"
+      )
+
+TextOnStaticMap(mm, 
+                lat = dc2$lat.m, lon = dc2$lon.m,
+                add = T,
+                labels = paste("N =", dc2$n),
+                cex = 0.5
+                )
+
+dev.print(device = png, 
+          width = 600,
+          height = 600,
+          file = paste0("Mapping_Route12_Clusters.png"))
+
+
+# 2. Kernal density ----
+
+
+
