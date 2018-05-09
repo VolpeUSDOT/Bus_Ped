1a56570211a6627abdf5102ab0b8465f5b6408a4
dflynn-volpe <daniel.flynn.ctr@dot.gov>
2018-05-09 14:14:06 -0400

    Final

--- Route_12_kde_matchCrimeStat.R
+++ Route_12_kde_matchCrimeStat.R
@@ -1,15 +1,11 @@
 # Pedestrian conflict hot spot analysis
 # Based on Mobileye data from vehicle in Seattle in April-June 2016
 # Using 
-# 1. Clustering
-# 2. Kernal density using ks package or other
+# 1. Clustering with hclust and cluster package
+# 2. Kernal density using spatstat or ks package
 
-# To match the CrimeStat/ArcGIS approach by Don and Andy
-
-# For each approach, make maps with satellite image overlays. Next step: Highlight bus stops, and look at associate between hot spots and bus stop.
-
-# Setup ----
-# If you don't have these packages: install.packages(c("maps", "sp","spatstat", "RgoogleMaps","ks","scales","tidyverse")) 
+# 5.1 Setup ----
+# If you don't have these packages: install.packages(c("maps", "sp","spatstat", "RgoogleMaps","ks","scales","tidyverse","cluster")) 
 library(maps)
 library(sp)
 library(spatstat)
@@ -17,32 +13,24 @@ library(RgoogleMaps)
 library(ks)
 library(scales)
 library(tidyverse)
+library(rgdal)
+library(cluster)
 
+# Set working directory and read in data
+#setwd("~/Documents/Work/Volpe/KDE") # update as appropriate 
 setwd("M:/Helping/Fisher/Crimestat") # update as appropriate 
 
 d <- read.csv("Route12Data041718.txt", stringsAsFactors = F, sep = "\t")
 
-PLOTMAPS = F # Set to T to make individual maps by StatusName
-
-# Summary of decisions from Don's writeup:
-# Bounding box: 47.599011 / -122.339481
-#               47.6341  / -122.303993
-
-# Should cover 4.358 sq mi
-
-# Neighbors: Nearest negibhro heierarchical spatial clustering and Random NN distance. 
-# Search radius in 'middle notch'
-# 1 SD for ellipses. 0 simulations. 10 points per cluster minimum. 
-
-# Look at the data and reformat ----
+# Look at the data and reformat
 
 summary(d)
 
-# get date and time in the right format
+# Get date and time in the right format
 d$time <- strptime(d$LocationTime, "%m/%d/%Y %H:%M:%S")
 summary(d$time)
 
-# trim trailing white space from all text fields
+# Trim trailing white space from all text fields
 textfields <- c("VehicleName", "UnitID", "Address", "StatusName")
 d[textfields] <- apply(d[textfields], 2, FUN = function(x) sub(" +$", "", x))
 
@@ -50,10 +38,14 @@ d[textfields] <- apply(d[textfields], 2, FUN = function(x) sub(" +$", "", x))
 d <- SpatialPointsDataFrame(coords = d[c("Longitude","Latitude")], data = d[c("Address", "StatusName", "time")],
                             proj4string = CRS("+proj=longlat +datum=WGS84"))
 
+# use albers equal area projection
+proj <- showP4(showWKT("+init=epsg:102008"))
+d.proj <- spTransform(d, CRS(proj))
+
 # Get data frame for plotting 
 dc <- data.frame(d@data, lat = coordinates(d)[,2], lon = coordinates(d)[,1])
 
-# 1. Clusters ----
+# 5.2 Cluster analysis ----
 
 # Parameters:
 min.cluster = 10 # minimum number of values in a cluster.
@@ -61,72 +53,122 @@ min.cluster = 10 # minimum number of values in a cluster.
 d.dist <- spDists(d, longlat = T)*1000 # spDists gives distance in km, increasing here to m 
 d.clust <- hclust(as.dist(d.dist), method = "single") # average: linkage by unweighted pairwise group method with arithmatic mean, UPGMA. Change method to "single" for single linkage, see ?hclust for more options. Single linkage is likely the what Crimestat refers to as nearest neighbor
 
-# cut the tree to make groupings
-# Find the number of groupings which results in no less than 10 values in a cluster
-# Search for correct k in a loop
-min.cluster.members = 0
-h = 1
+# The cluster package can identify optimal number of clusters, as an alternative to simply setting a minimum number of values in a cluster. Here the popular Gap statistic is used to identify the optimum number of clusters, created by k-means clustering instead of the single-linkage hierarchical clustering above.
+# This step may take several minutes; set "doGap" to TRUE to run this step.
+doGap = FALSE
 
-while(min.cluster.members < min.cluster){
-  d.cut <- cutree(d.clust, h = h)
-  min.cluster.members = min(table(d.cut))
-  h = h + 1
-  print(data.frame(k, min.cluster.members))
+if(doGap){
+  gap.metric.km <- clusGap(d.proj@coords, FUNcluster = kmeans, 
+                        K.max = 100,
+                        B = 100, verbose = interactive())
+  plot(gap.metric.km)
+  k <- maxSE(gap.metric.km$Tab[, "gap"], gap.metric.km$Tab[, "SE.sim"], method="Tibs2001SEmax")
+  cat("Gap estimate for optimal number of clusters:", k)
   }
-print(k)
 
-dc <- data.frame(d@data, d.cut, lat = coordinates(d)[,2], lon = coordinates(d)[,1], m.of.d)
+d.cut <- cutree(d.clust, k = 100)
 
-dc$time <- as.character(dc$time) # dplyr doesn't work well with date-time formats
+# Cluster names to not show, as these are below the minimum number of members:
+less.than.min = names(table(d.cut))[table(d.cut) <= min.cluster]
 
-# plot(as.dendrogram(d.clust))
-# rect.hclust(d.clust, k = 10)
+dc <- data.frame(d@data, d.cut, lat = coordinates(d)[,2], lon = coordinates(d)[,1])
 
-# aggregated by group and summarize. can add StatusName to group_by
+dc$time <- as.character(dc$time) # dplyr doesn't work well with date-time formats, so convert to text here.
+
+# Aggregated by group and summarize. Can add StatusName to group_by
+# Omit clusters which are fewer than the minimum number of members to show using filter() statement
 dc2 <- dc %>%
   group_by(d.cut) %>%
   summarize(lat.m = mean(lat),
             lon.m = mean(lon),
             lat.sd = sd(lat),
             lon.sd = sd(lon),
-            n = n(),
-            time = mean(m.of.d),
-            time.sd = sd(m.of.d)
-  )
+            n = n()
+  ) %>%
+  filter(!d.cut %in% less.than.min)
 
 mm <- plotmap(lat = dc2$lat.m, lon = dc2$lon.m,
               pch = 21,
-              bg = "grey80",
+              bg = "purple",
               lwd = 2,
-              cex = dc2$n/50,
-              col = "red",
+              cex = log(dc2$n/15, base = 2),
+              col = "black",
               maptype = "roadmap"
 )
 
+legend("topleft",
+       pt.bg = "purple",
+       col = "black",
+       pch = 21,
+       pt.cex = c(1, 1.7, 2.8, 3.9, 5.1),#levels(cut(log(dc2$n/15, base = 2), 5))
+       legend = plyr::round_any(15*(2^c(1, 1.7, 2.8, 3.9, 5.1)), 5),
+       title = "No. incidents",
+       y.intersp = 2,
+       x.intersp = 2)
+
+# Optional: show number of values in each cluster:
+ShowVals = FALSE
+if(ShowVals){
 TextOnStaticMap(mm, 
                 lat = dc2$lat.m, lon = dc2$lon.m,
                 add = T,
                 labels = paste("N =", dc2$n),
                 cex = 0.5
 )
-
+}
 dev.print(device = png, 
           width = 600,
           height = 600,
           file = paste0("Mapping_Route12_Clusters.png"))
 
 
-# 2. Kernal density ----
+# Convex Hulls ----
+# Calculate hulls for each distinct set of points deterimined by the d.cut of the clustering.
+# Loop over sets of points and apply the chull() function, then use this to create SpatialPolygons based on those values.
+
+hulllist = list()
+for(i in dc2$d.cut){ # i = 1
+   cv <- chull(dc[c("lon","lat")][d.cut == i,])
+   hulllist[[as.character(i)]] = Polygons(list(Polygon(dc[c("lon","lat")][d.cut == i,][cv,])), i)
+}
+
+ConvHullPoly <- SpatialPolygons(hulllist, proj4string = CRS(proj))
+
+# First, show all convex hull polygons:
+
+mm <- plotmap(lat = dc$lat, lon = dc$lon,
+              pch = "+",
+              cex = 0.8,
+              col = alpha("grey20", 0.5),
+              maptype = "road",
+              zoom = 14)
+
+PlotPolysOnStaticMap(mm, polys = ConvHullPoly, 
+                     col = alpha("lightgreen", 0.5))
+
+# Center on E. Madison and 17th and zoom in closer:
+
+mm <- GetMap(center = c(47.615669, -122.310151),
+              zoom = 17, GRAYSCALE = F,
+             maptype = 'road'
+             )
+PlotOnStaticMap(mm, lat = dc$lat, lon = dc$lon,
+              pch = 21,
+              bg = alpha("grey80", 0.8),
+              cex = 0.8,
+              col = alpha("grey20", 0.5))
+PlotPolysOnStaticMap(mm, polys = ConvHullPoly, 
+                     col = alpha("lightgreen", 0.8))
+
+# 5.3 Kernal density ----
 
 # Settings
-kern = "epanechnikov" #  "gaussian" # options to try: gaussian, epanechnikov, quartic and disc
-bwidth = 0.5 # betwen 0 and 1
-dataset = "all" # options: all, weekend, am, pm
+kern = "gaussian" # Kernel to use
+bwidth = 0.2 # between 0 and 1
 
 par.reset = par(no.readonly = T)
 
-# Get a map for plotting. Save as object "mm", which we will use for convertting lat longs into plottable points 
-
+# Get a map for plotting. Save as object "mm", which we will use for converting lat longs into plottable points 
 mm <- plotmap(lat = dc$lat, lon = dc$lon,
               pch = 1,
               col = alpha("white", 0),
@@ -136,92 +178,94 @@ mm <- plotmap(lat = dc$lat, lon = dc$lon,
 
 ll <- LatLon2XY.centered(mm, lat = coordinates(d)[,2], lon = coordinates(d)[,1])
 
-if(dataset == "Weekend") {
-      selector = format(dc$time, "%u") %in% c(6, 0) # weekday as decimal number, 0-6, Sunday is 0. Compare with %a
-      ll <- LatLon2XY.centered(mm, lat = coordinates(d)[,2][selector], lon = coordinates(d)[,1][selector])
-      if(!any(selector)) stop("No weekend days in this selection")
-      }
+# Can change f to higher value to extend the range of the observation window. Use the same observation window for all subsets of data.
+pwin = owin(xrange = extendrange(range(ll$newX), f = 0.2), 
+            yrange = extendrange(range(ll$newY), f = 0.2))
 
-if(dataset == "am") {
-    selector = format(dc$time, "%p") == "AM" #  AM/PM 
-    ll <- LatLon2XY.centered(mm, lat = coordinates(d)[,2][selector], lon = coordinates(d)[,1][selector])
-    if(!any(selector)) stop("No morning times in this selection")
-    }
+# Or manually set the observation window, following the CrimeStat approach
+ll.owin <- LatLon2XY.centered(mm, lat = c(47.599011, 47.635341), 
+                             lon = c(-122.339481, -122.303993))
+
+pwin = owin(xrange = range(ll.owin$newX), yrange = range(ll$newY))
+
+dp <- ppp(ll$newX, ll$newY, window = pwin) 
+
+# See ?density.ppp
+dd <- density(dp,
+              kernel = kern,
+              adjust = bwidth, # use this to change the bandwidth
+              edge = T, # adjustment for edge of observation window, recommend T
+              at = "pixels") # change to "points" to get the density exactly for each point
+
+# Define the color map:
+# make color map with increasing transparency at lower range
+coln = 5*25 # make it divisible by 3 for following steps
+col1 = rev(heat.colors(coln, alpha = 0.2))
+col2 = rev(heat.colors(coln, alpha = 0.8))
+col3 = rev(heat.colors(coln, alpha = 0.9))
+
+col4 = c(col1[1:coln/3], col2[(coln/3+1):(2*coln/3)], col3[(1+2*coln/3):coln])
+
+pc <- colourmap(col = col4, 
+                range = range(dd))
+
+# Or, use color map to match CrimeStat
+use.breaks = quantile(dd, probs = c(0.9, 0.95, 0.975, 0.99, 0.999, 1))
+pc <- colourmap(col = c(alpha("white",0.1),
+                alpha("yellow", 0.5),
+                alpha("red", 0.6),
+                alpha("purple", 0.7),
+                alpha("blue", 0.7)),
+                breaks = use.breaks)
 
 
-if(dataset == "pm") {
-    selector = format(dc$time, "%p") == "PM" # AM/PM 
-    ll <- LatLon2XY.centered(mm, lat = coordinates(d)[,2][selector], lon = coordinates(d)[,1][selector])
-    if(!any(selector)) stop("No evening times in this selection")
-    }
-
-# See ?kde. Normal kernel is default
-dl <- data.frame(ll$newX, ll$newY)
-dd <- kde(x = dl,
-          H = Hscv(dl),
-          adj.positive = 0.25)
-
-# Plotting ----
+# Plotting Kernel Density ----
 mm <- plotmap(lat = dc$lat, lon = dc$lon,
-              pch = "+",
-              cex = 0.8,
-              col = alpha("grey20", 0.05),
+              pch = 1,
+              col = alpha("white", 0),
               maptype = "roadmap")
 
-plot(dd, add = T, 
-     drawpoints = F,
-     display = "filled.contour")
+plot(dd, add = T, col = pc)
 
 # Get the contour levels
-levs <- contourLevels(dd, prob = c(0.25, 0.5, 0.75))
-contcut <- cut(dd$cont, breaks = levs)
-# select the first instance of each contour level
-colordf <- data.frame(contcut, heat.colors(99))
-legcol <- colordf[!duplicated(colordf[,1]),]
-
-legend("topleft",
-       title = "Density",
-       legend = legcol$contcut,
-       fill = as.character(legcol$heat.colors.99))
+# select the color for of each contour level
+legcol <- pc(use.breaks)
 
 # Pixel values are estimated intensity values, expressed in "points per unit area".
-
-legend("top", legend = paste(toupper(dataset), kern, bwidth))
-
-dev.print(device = png, 
-          width = 600,
-          height = 600,
-          file = paste0(paste("Mapping_Rt12", kern, bwidth, dataset, sep = "_"), ".png"))
+legend("topleft",
+       title = "Density",
+       legend = round(use.breaks, 4),
+       fill = legcol)
 
 plot(dd); dev.print(device = png, 
                    width = 600,
                    height = 600,
                    file = paste0(paste("Unmapped", kern, bwidth, dataset, sep = "_"), ".png"))
 
-# Difference between am / pm intensities ----
-
-selector = format(dc$time, "%p") == "AM" #  AM/PM 
-ll <- LatLon2XY.centered(mm, lat = coordinates(d)[,2][selector], lon = coordinates(d)[,1][selector])
-dp <- ppp(ll$newX, ll$newY, window = pwin)
-dd.am <- density(dp, kernel = kern, adjust = bwidth, edge = T)
-
-selector = format(dc$time, "%p") == "PM" # AM/PM 
-ll <- LatLon2XY.centered(mm, lat = coordinates(d)[,2][selector], lon = coordinates(d)[,1][selector])
-dp <- ppp(ll$newX, ll$newY, window = pwin)
-dd.pm <- density(dp, kernel = kern, adjust = bwidth, edge = T)
-
-dd.diff <- dd.pm - dd.am
-
-par(par.reset)
-plot(dd.diff, main = paste("PM - AM densities \n", kern, bwidth))
-dev.print(device = png, 
-          width = 600,
-          height = 600,
-          file = paste0(paste("Unmapped_PM-AM", kern, bwidth, sep = "_"), ".png"))
+# Combine kernel density, convex hulls, and points in zoomed in map.
+# Make a high resoltion map and zoom in by cropping
 
 
-# titleloc <- XY2LatLon(mm, 2.221, 277.31) # use locator() to find where to put title
-# TextOnStaticMap(mm, lat = titleloc[1], lon = titleloc[2], 
-#                 paste(dataset, kern, bwidth), 
-#                 add = T, pch = 2,
-#                 font = 2)
+mm <- plotmap(lat = dc$lat, lon = dc$lon,
+              pch = 1,
+              col = alpha("white", 0),
+              maptype = "roadmap")
+
+# pdf("OverlayMap.pdf", width = 20, height = 20) # uncomment for PDF
+mm <- GetMap.bbox(lonR = range(dc$lon),
+                  latR = range(dc$lat),
+            zoom = 14,
+            SCALE = 2,
+             maptype = 'road'
+             )
+
+PlotOnStaticMap(mm, lat = dc$lat, lon = dc$lon,
+              pch = 21,
+              bg = alpha("grey80", 0.8),
+              cex = 0.8,
+              col = alpha("grey20", 0.5))
+PlotPolysOnStaticMap(mm, polys = ConvHullPoly, 
+                     col = alpha("lightgreen", 0.8))
+
+spatstat::plot.im(dd, col = pc, add = T)
+# dev.off() # uncomment for PDF output
