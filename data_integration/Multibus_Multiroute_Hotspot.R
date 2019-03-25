f563fad051e0136071964fcb393ad80710edf0d4
dflynn-volpe <daniel.flynn@dot.gov>
2019-03-25 08:40:05 -0400

    Updated heat map figures

--- data_integration/Multibus_Multiroute_Hotspot.R
+++ data_integration/Multibus_Multiroute_Hotspot.R
@@ -32,35 +32,33 @@ library(doParallel)
 codeloc = "~/git/Bus_Ped/"
 rootdir <- "//vntscex.local/DFS/3BC-Share$_Mobileye_Data/Data/"
 Database = "ituran_synchromatics_data.sqlite" # select version of database to use
-bandwithdth_adj = 0.8 # proportion to adjust the bandwidth of the kernel density estimates. at 1, uses the default from bandwidth.nrd
+bandwithdth_adj = 0.5 # proportion to adjust the bandwidth of the kernel density estimates. at 1, uses the default from bandwidth.nrd
 # Set warning type or types to analyze. Braking events in June: 469 aggressive, 34 dangerous 
 warning_keywords = c('All', 'Braking', 'PCW', 'PDZ') # One word. E.g. Braking, PCW, PDZ, ME
 # Set month to operate over
-use_month = '2018-06'
+use_month = '2018-06' # not yet implemented, use this to pick out month of interest. Only June 2018 currently available
 # <><><><><><><><><><><><><><><><><><><><>
 
 setwd(rootdir)
 version = paste(gsub("\\.sqlite", "", Database), 'version', sep = '_')
 
-load(file.path(version, "Temp_Event_Dist_Nearest_byHour_DASH.RData"))
+load(file.path(version, "Temp_Event_Dist_Nearest_byHour_DASH.RData")) # loads data.frame db_2, which includes the  nearest.route variable from the proximity analysis.
 
-# Load shapefiles
+# Load shapefiles and projection
 if(length(grep('LADOT_routes.RData', dir('Routes'))) == 0) {
   source(file.path(codeloc, "Route_prep.R")) 
 } else { 
   if(!exists("dt_dash")) load(file.path('Routes', "LADOT_routes.RData"))
 }
 
-d <- db_2
-
 # Before looping over each warning, set up values of whole grid area to analyze. Make this the same for every layer.
-d <- SpatialPointsDataFrame(coords = d[c("longitude","latitude")], data = d, proj4string = CRS(proj))
+d_all <- SpatialPointsDataFrame(coords = db_2[c("longitude","latitude")], data = db_2, proj4string = CRS(proj))
 
 # current area in square kilometers. Extend the range by 10% to cover a wider range
-use_lon_range = extendrange(d@coords[,1], f = 0.1)
-use_lat_range = extendrange(d@coords[,2], f = 0.1)
+use_lon_range = extendrange(d_all@coords[,1], f = 0.1)
+use_lat_range = extendrange(d_all@coords[,2], f = 0.1)
 
-( total_area_sqkm = diff(use_lon_range)/1000 * diff(use_lat_range)/1000 )
+total_area_sqkm = diff(use_lon_range)/1000 * diff(use_lat_range)/1000
 
 # Make each grid cell 50 sq m. So each side needs to be sqrt(50) 
 # longitudinal count of grids to use:
@@ -71,15 +69,15 @@ lat_n_use = ceiling(diff(use_lat_range) / sqrt(50) )
 
 for(warningtype in warning_keywords){ # Start loop over warning types. warningtype = 'PCW'
   
-  usewarning = unique(d$warning_name)[grep(warningtype, unique(d$warning_name))]
+  usewarning = unique(d_all$warning_name)[grep(warningtype, unique(d_all$warning_name))]
   
-  if(warningtype == 'All') { usewarning = unique(d$warning_name)}
+  if(warningtype == 'All') { usewarning = unique(d_all$warning_name)}
   if(warningtype == 'PCW') { usewarning = c(usewarning, 'ME - Pedestrian Collision Warning')}
   if(warningtype == 'PDZ') { usewarning = c(usewarning, 'ME - Pedestrian In Range Warning')}
   
   # Make route_id name with heading and filter out mismatches for now. Drop a bunch of unneeded columns.
   # Now also applying the warning type filter
-  d = d@data %>%
+  d = d_all@data %>%
     mutate(route_id = make.names(paste(route_name, heading)),
            prox_assigned = paste("DASH", nearest.route),
            mismatch = prox_assigned != route_name) %>%
@@ -92,9 +90,6 @@ for(warningtype in warning_keywords){ # Start loop over warning types. warningty
   d <- SpatialPointsDataFrame(coords = d[c("longitude","latitude")], data = d %>% dplyr::select(-longitude, -latitude),
                               proj4string = CRS(proj))
   
-  # Make a lat long projection 
-  d_ll_proj = spTransform(d, CRS("+proj=longlat +datum=WGS84"))
-  
   # Work in parallel
   cl = makeCluster(parallel::detectCores())
   registerDoParallel(cl)
@@ -102,8 +97,8 @@ for(warningtype in warning_keywords){ # Start loop over warning types. warningty
   # Kernel density estimation from MASS ----
   # We want to translate from density per degree to count per square mile or other more easily interpretable units. So we need to work on projected data, not in units of decimal degrees. Good explanation here: https://www.esri.com/arcgis-blog/products/product/analytics/how-should-i-interpret-the-output-of-density-tools/
   
-  foreach(idx = unique(d_ll_proj$route_id), .packages = c("dplyr", "ggmap", 'MASS', 'raster', 'sp', 'reshape')) %dopar% { 
-    # idx = unique(d_ll_proj$route_id)[1]
+  foreach(idx = unique(d$route_id), .packages = c("dplyr", "ggmap", 'MASS', 'raster', 'sp', 'reshape')) %dopar% { 
+    # idx = unique(d$route_id)[1]
     usedat = d@coords[ d@data$route_id == idx, ]
     
     dens = kde2d(usedat[,1], usedat[,2],
@@ -117,8 +112,8 @@ for(warningtype in warning_keywords){ # Start loop over warning types. warningty
     
     dens$z = ( dens$z / total_area_sqkm ) * 1e6  
   
-    # Set bottom 25th of the percentile to NA
-    dens$z[dens$z <= quantile(dens$z, 0.25)] = NA
+    # Set bottom 75th of the percentile to NA
+    dens$z[dens$z <= quantile(dens$z, 0.75)] = NA
     
     # Make density raster into a spatial object
     dimnames(dens$z) = list(dens$x, dens$y)
@@ -171,8 +166,8 @@ for(warningtype in warning_keywords){ # Start loop over warning types. warningty
     mutate(rte_head = make.names(paste(route_name, heading)))
   
   # Now re-do the heat map, dividing by month_trip_count for each route name and heading combination
-    foreach(idx = unique(d_ll_proj$route_id), .packages = c("dplyr", "ggmap", 'MASS', 'raster', 'sp', 'reshape')) %dopar% { 
-    # idx = unique(d_ll_proj$route_id)[1]
+    foreach(idx = unique(d$route_id), .packages = c("dplyr", "ggmap", 'MASS', 'raster', 'sp', 'reshape')) %dopar% { 
+    # idx = unique(d$route_id)[1]
     cat(idx, '\n')
     
     usedat = d@coords[ d@data$route_id == idx, ]
@@ -188,13 +183,13 @@ for(warningtype in warning_keywords){ # Start loop over warning types. warningty
     
     dens$z = ( dens$z / total_area_sqkm ) * 1e6  
     
-    # Set bottom 25th of the percentile to NA
-    dens$z[dens$z <= quantile(dens$z, 0.25)] = NA
+    # Set bottom 75th of the percentile to NA
+    dens$z[dens$z <= quantile(dens$z, 0.75)] = NA
     
     # Now standardize by dividing by the total number of trips. Use the data frame month_trip_count.
     count_i = month_trip_count %>% ungroup() %>% filter(rte_head == idx) %>% dplyr::select(count)
     
-    dens$z = dens$z / as.numeric(count_i)
+    dens$z = dens$z / as.numeric(count_i) * 100 # making it per 100 trips for better units
     
     # Make density raster into a spatial object
     dimnames(dens$z) = list(dens$x, dens$y)
