1a4cb05149c34c9b01677d293e03662f83235def
jshiu-volpe <janice.shiu@dot.gov>
2019-06-25 18:03:32 -0400

    Renamed and reorganized files. Added .tif file to .lyr file maker written as Layer_Maker.py

--- Maps/Layer_Maker.py
+++ Maps/Layer_Maker.py
@@ -6,46 +6,43 @@ Created on Mon Jun 24 14:46:53 2019
 """
 
 import arcpy
-print("running")
-#Start with a map document
-mxd = arcpy.mapping.MapDocument("CURRENT")
-mxd.author = "Volpe"
+import os
+import shutil
 
-#Add Route. Will always be at the top of the layers list 
-routeb = arcpy.mapping.Layer(r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\Data\Routes\Route_Layers\Route_B.lyr')
-df = arcpy.mapping.ListDataFrames(mxd, "Layers")[0]
-arcpy.mapping.AddLayer(df,routeb)
+months = ["Jan", "Feb", "Mar", "Apr","May","Jun","Jul",
+          "Aug", "Sep", "Oct", "Nov", "Dec", "Ras", "Hot"]
+normalization = ["Normalized", "Unnormalized"]
+datatypes = ["All", "Braking", "PCW","PDZ"]
+routes = ["A", "B", "D", "E", "F"]
 
-print("Add basemap")
-#Add Basemap. Will always be at the bottom of the layers list
-basemapLayer = arcpy.mapping.Layer(r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\ArcMap\Basemap.lyr')   
-df = arcpy.mapping.ListDataFrames(mxd, "*")[0]
-arcpy.mapping.AddLayer(df, basemapLayer, "BOTTOM")
+arcmappath = r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\ArcMap'
+datapath = r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\Data'
 
-print("Add Raster")
-#Add layer of raster data
-rastlyr = arcpy.mapping.Layer(r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\ArcMap\Raster_Unnormalized_All_DASHtest.lyr')
-df = arcpy.mapping.ListDataFrames(mxd, "*")[0]
-'''
-#Get metadata to set the name and to find whether the is a matching dataset in
-#the cluster data
-file_decomp = file.split("_")
-mon = file_decomp[0][0:3]
-if mon == "Ras":
+rasternest=["/"+m +"/"+n+"/"+d+"/"+r+"/" for m in months for d in datatypes for r in routes for n in normalization]
+clusternest=["/"+m +"/"+d+"/"+r+"/" for m in months for d in datatypes for r in routes]
+
+if "Raster_Sorted_Layers" in os.listdir(arcmappath):
+    shutil.rmtree(arcmappath + "/Raster_Sorted_Layers")
+#    if "Cluster_Sorted_Layers" in os.listdir(arcmappath):
+#        shutil.rmtree(arcmappath + "/Cluster_Sorted_Layers")
     
-dtype = datatypes[lc_datatype.index(file_decomp[2].lower())]
-shutil.copy(src = path+"/Cluster/"+ file, dst = path+"/Cluster_Sorted/"+mon+"/"+dtype)
-'''
-rastlyr.name = "TEMP"
-arcpy.mapping.AddLayer(df,rastlyr)
+std_raster = arcpy.mapping.Layer(arcmappath + '\Standard Layers\Standard_Raster.lyr')
+std_cluster = arcpy.mapping.Layer(arcmappath + '\Standard Layers\Standard_Cluster.lyr')    
+for ra in rasternest:
+    os.makedirs(arcmappath+"/Raster_Sorted_Layers/"+ra)
+for ra in rasternest:
+    for f in os.listdir(datapath+"/Raster_Sorted"+ra):
+        print(f)
+        if f.endswith(".tif"):
+            newlyr = arcpy.mapping.Layer(datapath+"/Raster_Sorted" + ra+f)
+            newlyr.saveACopy(arcmappath+"/Raster_Sorted_Layers"+ra+f)
+#        std_raster.replaceDataSource(datapath+"/Raster_Sorted" +ra, "NONE",f)
+#        std_raster.name = f
+#        std_raster.saveACopy(arcmappath +"/Raster_Sorted"+ra+f)
 
-print("standardize")
-#Update Raster Layer to standard
-updateLayer = arcpy.mapping.ListLayers(mxd, "", df)[1]
-sourceLayer = arcpy.mapping.Layer(r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\ArcMap\Standard_Raster.lyr')
-arcpy.mapping.UpdateLayer(df, updateLayer, sourceLayer, True)
-
-#Add layer of cluster data
-
-#How to make a raster layer from a file
-#test = arcpy.MakeRasterLayer_management(r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\Data\Raster\AprilRaster_Unnormalized_Braking_DASH.A.N.tif', "test")
+#    for cl in clusternest:
+#        os.makedirs(arcmappath+"/Cluster_Sorted_Layers"+cl)
+#        for f in os.listdir(datapath+cl):
+#            std_cluster.replaceDataSource(datapath+"/Cluster_Sorted" +cl, "NONE",f)
+#            std_cluster.name = f
+#            std_cluster.saveACopy(arcmappath +"/Cluster_Sorted"+cl+f)
--- Maps/Reorganize_Data.py
+++ Maps/Reorganize_Data.py
@@ -6,14 +6,13 @@ Created on Mon Jun 24 13:44:17 2019
 """
 import os
 import shutil
-
+path = r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\Data'
 #Reorganize data into folders by month and normalization
-path = "\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\Data"
 
 #Remove any old folders
-#if "Raster_Sorted" in os.listdir():
+#if "Raster_Sorted" in os.listdir(path):
 #    shutil.rmtree(path + "/Raster_Sorted")
-if "Cluster_Sorted" in os.listdir():
+if "Cluster_Sorted" in os.listdir(path):
     shutil.rmtree(path + "/Cluster_Sorted")
     
 #Create filesystem
@@ -21,13 +20,15 @@ months = ["Jan", "Feb", "Mar", "Apr","May","Jun","Jul",
           "Aug", "Sep", "Oct", "Nov", "Dec", "Ras", "Hot"]
 normalization = ["Normalized", "Unnormalized"]
 datatypes = ["All", "Braking", "PCW","PDZ"]
+routes = ["A", "B", "D", "E", "F"]
 
 for m in months:
     for d in datatypes:
-        os.makedirs(path + "/Cluster_Sorted/" + m + "/" + d)
-        for n in normalization:
-#            os.makedirs(path + "/Raster_Sorted/" + m + "/" + n + "/" + d)
-            continue
+        for r in routes :
+            os.makedirs(path + "/Cluster_Sorted/" + m + "/" + d + "/" + r)
+            for n in normalization:
+#                os.makedirs(path + "/Raster_Sorted/" + m + "/" + n + "/" + d + "/" + r)
+                continue
 
 #Have a lowercase version of datatypes so we can better identify the types
 #Because the name of the datatype is inconsistent across files
@@ -35,17 +36,23 @@ datatypes = ["All", "Braking", "PCW","PDZ"]
 lc_datatype = ["all", "braking", "pcw","pdz"]
 dtypes = []
 
-
+'''
 ##Sort Raster Data
-#for file in os.listdir(path + "\Raster"):
-#    file_decomp = file.split("_")
-#    mon = file_decomp[0][0:3]
-#    norm = file_decomp[1]
-#    dtype = datatypes[lc_datatype.index(file_decomp[2].lower())]
-#    shutil.copy(src = path+"/Raster/"+file,
-#                    dst = path+"/Raster_Sorted/"+mon+"/"+norm+"/"+dtype)
-
-
+for file in os.listdir(path + "\Raster"):
+    file_decomp = file.split("_")
+    mon = file_decomp[0][0:3]
+    try:
+        norm = file_decomp[1]
+    except:
+        break
+    dtype = datatypes[lc_datatype.index(file_decomp[2].lower())]
+    try:
+        route = routes[routes.index(file_decomp[3].split(".")[-3])]
+    except:
+        print(file)
+    shutil.copy(src = path+"/Raster/"+file,
+                    dst = path+"/Raster_Sorted/"+mon+"/"+norm+"/"+dtype+ "/" + route)
+'''
 #Sort Cluster Data
 for file in os.listdir(path + "\Cluster"):
     try:
@@ -54,7 +61,8 @@ for file in os.listdir(path + "\Cluster"):
             file_decomp = file.split("_")
             mon = file_decomp[0][0:3]
             dtype = datatypes[lc_datatype.index(file_decomp[2].lower())]
-            shutil.copy(src = path+"/Cluster/"+ file, dst = path+"/Cluster_Sorted/"+mon+"/"+dtype)
+            route = routes[routes.index(file_decomp[3].split(".")[-3])]
+            shutil.copy(src = path+"/Cluster/"+ file, dst = path+"/Cluster_Sorted/"+mon+"/"+dtype+"/" + route)
     except:
         print(file)
         break
