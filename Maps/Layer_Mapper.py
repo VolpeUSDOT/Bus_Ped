5e6cd8aad9b56b88748ea07fc1f688b8509e4c52
jshiu-volpe <janice.shiu@dot.gov>
2019-06-26 12:33:44 -0400

    Generalized variables to make iteration possible

--- Maps/Layer_Mapper.py
+++ Maps/Layer_Mapper.py
@@ -4,51 +4,99 @@ Created on Tue Jun 25 17:22:06 2019
 
 @author: Janice.Shiu
 """
-
-def make_map():
-    rasternest=["/"+m +"/"+n+"/"+d+"/"+r+"/" for m in months for d in datatypes for r in routes for n in normalization]
+import arcpy 
+import os
+def make_map(params):
+    arcmappath =r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\ArcMap'
     
-    
-    print("running")
     #Start with a map document
-    mxd = arcpy.mapping.MapDocument("CURRENT")
+    mxd = mxd = arcpy.mapping.MapDocument(arcmappath + '\StandardMap.mxd')
     mxd.author = "Volpe"
-    
-    #Add Route. Will always be at the top of the layers list 
-    routeb = arcpy.mapping.Layer(r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\ArcMap\Standard Layers\Route_B.lyr')
-    df = arcpy.mapping.ListDataFrames(mxd, "Layers")[0]
-    arcpy.mapping.AddLayer(df,routeb)
-    
-    print("Add basemap")
+    for p in params:
+        #p = (str interaction type, str route letter, str N or S, str month, bool plot raster, bool plot clust, bool normalized)
+        d, r, NoS, m, ras, clust, norm = p
+        
+        
+        #Add Route. Will always be at the top of the layers list 
+        route = arcpy.mapping.Layer(arcmappath + 'Standard Layers\Route_'+ r +'.lyr')
+        df = arcpy.mapping.ListDataFrames(mxd, "Layers")[0]
+        arcpy.mapping.AddLayer(df,route)
+        '''
+        #Set Scale
+        for df in arcpy.mapping.ListDataFrames(mxd):
+            df.scale = 24000
+        '''
+        
+        #Add layer of raster data
+        if ras == True:
+            try:
+                n = "Normalized" if norm == True else "Unnormalized"
+                filepath =arcmappath + '/Raster_Sorted_Layers/'+"/"+m +"/"+n+"/"+d+"/"+r
+                
+                #Find the right file to map and map it
+                for f in os.listdir(filepath):
+                    if f.split(".")[-2] == NoS:
+                        #Create layer
+                        rastlyr = arcpy.mapping.Layer(filepath+"/" + f)
+                        
+                        #Format layer name
+                        space = " "
+                        rastlyr.name = space.join([n, d, "DASH", r, "-", NoS, m])
+                        
+                        #Map layer
+                        arcpy.mapping.AddLayer(df,rastlyr)
+
+                        #Update layer to standard symbology
+                        updateLayer = arcpy.mapping.ListLayers(mxd, "", df)[1]
+                        sourceLayer = arcpy.mapping.Layer(arcmappath + 
+                                                          '\Standard Layers\Standard_'+n+'_Raster.lyr')
+                        arcpy.mapping.UpdateLayer(df, updateLayer, sourceLayer, True)                        
+            except:
+                #Print errors if raster file does not exist
+                comma = ", "
+                print("Raster file for "+ comma.join(["Datatype " + d,
+                                                      "Route " + r + NoS, m]) + "does not exist.")
+                answer = input("Proceed without layer? Y or N: ")
+                if answer == "N":
+                    print("Layer_Mapper.py Terminated")
+                    break
+        #Add cluster layer
+        if clust == True:
+            print("Cluster layers not available yet. Proceeding without plotting clusters.")
+            '''
+            try:
+                n = "Normalized" if norm == True else "Unnormalized"
+                filepath =arcmappath + '/Cluster_Sorted_Layers/'+"/"+m +"/"+d+"/"+r
+                
+                #Find the right file to map and map it
+                for f in os.listdir(filepath):
+                    if f.split(".")[-2] == NoS:
+                        #Create layer
+                        clustlyr = arcpy.mapping.Layer(filepath+"/" + f)
+                        
+                        #Format layer name
+                        space = " "
+                        clustlyr.name = space.join(["HOTSPOT", d, "DASH", r, "-", NoS, m])
+                        
+                        #Map layer
+                        arcpy.mapping.AddLayer(df,clustlyr)
+
+                        #Update layer to standard symbology
+                        updateLayer = arcpy.mapping.ListLayers(mxd, "", df)[1]
+                        sourceLayer = arcpy.mapping.Layer(arcmappath + 
+                                                          '\Standard Layers\Standard_Cluster.lyr')
+                        arcpy.mapping.UpdateLayer(df, updateLayer, sourceLayer, True)                        
+            except:
+                #Print errors if cluster file does not exist, but keep going
+                comma = ", "
+                print("Cluster file for "+ comma.join(["Datatype " + d,
+                                                      "Route " + r + NoS, m]) + "does not exist.")
+                print("Proceeding without layer.")
+            '''
+                
     #Add Basemap. Will always be at the bottom of the layers list
-    basemapLayer = arcpy.mapping.Layer(r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\ArcMap\Standard Layers\Basemap.lyr')   
-    df = arcpy.mapping.ListDataFrames(mxd, "*")[0]
+    basemapLayer = arcpy.mapping.Layer(arcmappath + '\Standard Layers\Basemap.lyr')
     arcpy.mapping.AddLayer(df, basemapLayer, "BOTTOM")
     
-    print("Add Raster")
-    #Add layer of raster data
-    rastlyr = arcpy.mapping.Layer(r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\ArcMap\Raster_Unnormalized_All_DASHtest.lyr')
-    df = arcpy.mapping.ListDataFrames(mxd, "*")[0]
-    '''
-    #Get metadata to set the name and to find whether the is a matching dataset in
-    #the cluster data
-    file_decomp = file.split("_")
-    mon = file_decomp[0][0:3]
-    if mon == "Ras":
-        
-    dtype = datatypes[lc_datatype.index(file_decomp[2].lower())]
-    shutil.copy(src = path+"/Cluster/"+ file, dst = path+"/Cluster_Sorted/"+mon+"/"+dtype)
-    '''
-    rastlyr.name = "TEMP"
-    arcpy.mapping.AddLayer(df,rastlyr)
-    
-    print("standardize")
-    #Update Raster Layer to standard
-    updateLayer = arcpy.mapping.ListLayers(mxd, "", df)[1]
-    sourceLayer = arcpy.mapping.Layer(r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\ArcMap\Standard Layers\Standard_Raster.lyr')
-    arcpy.mapping.UpdateLayer(df, updateLayer, sourceLayer, True)
-
-#Add layer of cluster data
-
-#How to make a raster layer from a file
-#test = arcpy.MakeRasterLayer_management(r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\Data\Raster\AprilRaster_Unnormalized_Braking_DASH.A.N.tif', "test")
+    legend = arcpy.mapping.ListLayoutElements(mxd, "LEGEND_ELEMENT", "Legend")[0]
+    legend.autoAdd = True
