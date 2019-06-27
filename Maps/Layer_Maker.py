25981fe9938c198ba322010065a4b0b99276f226
jshiu-volpe <janice.shiu@dot.gov>
2019-06-27 11:10:17 -0400

    Added code to rename files, create arcmap layers, and use the correct filepaths

--- Maps/Layer_Maker.py
+++ Maps/Layer_Maker.py
@@ -8,41 +8,49 @@ Created on Mon Jun 24 14:46:53 2019
 import arcpy
 import os
 import shutil
+import glob
 
+#Folder names for the nested directories
 months = ["Jan", "Feb", "Mar", "Apr","May","Jun","Jul",
           "Aug", "Sep", "Oct", "Nov", "Dec", "Ras", "Hot"]
 normalization = ["Normalized", "Unnormalized"]
 datatypes = ["All", "Braking", "PCW","PDZ"]
 routes = ["A", "B", "D", "E", "F"]
 
-arcmappath = r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\ArcMap'
+#Working paths for source(datapath) destination (arcmappath)
 datapath = r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\Data'
+arcmappath = r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\ArcMap'
 
-rasternest=["/"+m +"/"+n+"/"+d+"/"+r+"/" for m in months for d in datatypes for r in routes for n in normalization]
-clusternest=["/"+m +"/"+d+"/"+r+"/" for m in months for d in datatypes for r in routes]
+#Create directories for nested paths
+rasternest=[os.path.join(m,n,d,r) for m in months for d in datatypes for r in routes for n in normalization]
+clusternest=[os.path.join(m, d, r) for m in months for d in datatypes for r in routes]
 
+#Remove any old folders created
+'''
+print("Removing Folders")
 if "Raster_Sorted_Layers" in os.listdir(arcmappath):
-    shutil.rmtree(arcmappath + "/Raster_Sorted_Layers")
-#    if "Cluster_Sorted_Layers" in os.listdir(arcmappath):
-#        shutil.rmtree(arcmappath + "/Cluster_Sorted_Layers")
-    
-std_raster = arcpy.mapping.Layer(arcmappath + '\Standard Layers\Standard_Raster.lyr')
-std_cluster = arcpy.mapping.Layer(arcmappath + '\Standard Layers\Standard_Cluster.lyr')    
-for ra in rasternest:
-    os.makedirs(arcmappath+"/Raster_Sorted_Layers/"+ra)
-for ra in rasternest:
-    for f in os.listdir(datapath+"/Raster_Sorted"+ra):
-        print(f)
-        if f.endswith(".tif"):
-            newlyr = arcpy.mapping.Layer(datapath+"/Raster_Sorted" + ra+f)
-            newlyr.saveACopy(arcmappath+"/Raster_Sorted_Layers"+ra+f)
-#        std_raster.replaceDataSource(datapath+"/Raster_Sorted" +ra, "NONE",f)
-#        std_raster.name = f
-#        std_raster.saveACopy(arcmappath +"/Raster_Sorted"+ra+f)
+    shutil.rmtree(os.path.join(arcmappath, "Raster_Sorted_Layers"))
+if "Cluster_Sorted_Layers" in os.listdir(arcmappath):
+    shutil.rmtree(os.path.join(arcmappath,"Cluster_Sorted_Layers"))
+'''
 
-#    for cl in clusternest:
-#        os.makedirs(arcmappath+"/Cluster_Sorted_Layers"+cl)
-#        for f in os.listdir(datapath+cl):
-#            std_cluster.replaceDataSource(datapath+"/Cluster_Sorted" +cl, "NONE",f)
-#            std_cluster.name = f
-#            std_cluster.saveACopy(arcmappath +"/Cluster_Sorted"+cl+f)
+#Make directories for raster layers
+#Turn .tif raster files into arcmap layers
+print("Making Raster Layers")
+for ra in rasternest:
+    os.makedirs(os.path.join(arcmappath,"Raster_Sorted_Layers",ra))
+for ra in rasternest:
+    for f in os.listdir(os.path.join(datapath,"Raster_Sorted",ra)):
+        if f.endswith(".tif"):
+            newlyr = arcpy.mapping.Layer(os.path.join(datapath,"Raster_Sorted",ra,f))
+            newlyr.saveACopy(os.path.join(arcmappath,"Raster_Sorted_Layers",ra,f))
+
+#Make directories for cluster layers
+#Turn .shp cluster files into arcmap layers
+print("Making Cluster Layers")
+for cl in clusternest:
+    os.makedirs(os.path.join(arcmappath, "Cluster_Sorted_Layers", cl))
+for cl in clusternest:
+    for f in glob.glob(os.path.join(datapath, "Cluster_Sorted", cl, "*.shp")):
+        newlyr =  arcpy.mapping.Layer(f)
+        newlyr.saveACopy(os.path.join(arcmappath,"Cluster_Sorted_Layers",cl, os.path.basename(f)))
--- Maps/Reorganize_Data.py
+++ Maps/Reorganize_Data.py
@@ -10,12 +10,15 @@ path = r'\\vntscex.local\DFS\3BC-Share$_Mobileye_Data\Data'
 #Reorganize data into folders by month and normalization
 
 #Remove any old folders
-#if "Raster_Sorted" in os.listdir(path):
-#    shutil.rmtree(path + "/Raster_Sorted")
+
+print("Removing Folders")
+if "Raster_Sorted" in os.listdir(path):
+    shutil.rmtree(os.path.join(path,"Raster_Sorted"))
 if "Cluster_Sorted" in os.listdir(path):
-    shutil.rmtree(path + "/Cluster_Sorted")
-    
+    shutil.rmtree(os.path.join(path,"Cluster_Sorted"))
+
 #Create filesystem
+print("Creating Filesystem")
 months = ["Jan", "Feb", "Mar", "Apr","May","Jun","Jul",
           "Aug", "Sep", "Oct", "Nov", "Dec", "Ras", "Hot"]
 normalization = ["Normalized", "Unnormalized"]
@@ -25,10 +28,9 @@ routes = ["A", "B", "D", "E", "F"]
 for m in months:
     for d in datatypes:
         for r in routes :
-            os.makedirs(path + "/Cluster_Sorted/" + m + "/" + d + "/" + r)
+            os.makedirs(os.path.join(path,"Cluster_Sorted",m,d,r))
             for n in normalization:
-#                os.makedirs(path + "/Raster_Sorted/" + m + "/" + n + "/" + d + "/" + r)
-                continue
+                os.makedirs(os.path.join(path, "Raster_Sorted",m,n,d,r))
 
 #Have a lowercase version of datatypes so we can better identify the types
 #Because the name of the datatype is inconsistent across files
@@ -36,33 +38,39 @@ datatypes = ["All", "Braking", "PCW","PDZ"]
 lc_datatype = ["all", "braking", "pcw","pdz"]
 dtypes = []
 
-'''
-##Sort Raster Data
-for file in os.listdir(path + "\Raster"):
-    file_decomp = file.split("_")
-    mon = file_decomp[0][0:3]
-    try:
+
+#Sort Raster Data
+print("Sorting Raster Data")
+for file in os.listdir(os.path.join(path,"Raster")):
+    filename, fileext = os.path.splitext(file)
+    if fileext == ".tif":
+        #Remove periods from the name
+        filename = filename.replace(".", "_") + fileext
+        
+        #Decompose where the file belongs
+        file_decomp = filename.split("_")
+        mon = file_decomp[0][0:3]
         norm = file_decomp[1]
-    except:
-        break
-    dtype = datatypes[lc_datatype.index(file_decomp[2].lower())]
-    try:
-        route = routes[routes.index(file_decomp[3].split(".")[-3])]
-    except:
-        print(file)
-    shutil.copy(src = path+"/Raster/"+file,
-                    dst = path+"/Raster_Sorted/"+mon+"/"+norm+"/"+dtype+ "/" + route)
-'''
+        dtype = datatypes[lc_datatype.index(file_decomp[2].lower())]
+        route = file_decomp[4]
+        
+        #Copy the file into the sorted directory with the corrected name
+        shutil.copy(src = os.path.join(path,"Raster", file),
+                    dst = os.path.join(path, "Raster_Sorted", mon, norm, dtype, route, filename))
+
 #Sort Cluster Data
+print("Sorting Cluster Data")
 for file in os.listdir(path + "\Cluster"):
-    try:
-        if not file.endswith(".txt") and file != "Archive" and \
-        not file.endswith(".lock"):
-            file_decomp = file.split("_")
-            mon = file_decomp[0][0:3]
-            dtype = datatypes[lc_datatype.index(file_decomp[2].lower())]
-            route = routes[routes.index(file_decomp[3].split(".")[-3])]
-            shutil.copy(src = path+"/Cluster/"+ file, dst = path+"/Cluster_Sorted/"+mon+"/"+dtype+"/" + route)
-    except:
-        print(file)
-        break
+    filename, fileext = os.path.splitext(file)
+    if fileext == ".shp" or fileext == ".dbf" or fileext == ".prj" or fileext == ".shx":
+        #Remove periods from the name
+        filename = filename.replace(".","_") + fileext
+        
+        #Decompose where the file belongs
+        file_decomp = filename.split("_")
+        mon = file_decomp[0][0:3]
+        dtype = datatypes[lc_datatype.index(file_decomp[2].lower())]
+        route = file_decomp[4]
+        
+        shutil.copy(src = os.path.join(path, "Cluster", file),
+                    dst = os.path.join(path, "Cluster_Sorted", mon, dtype, route, filename))
